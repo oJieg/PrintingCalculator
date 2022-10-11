e5caffb3c027016f92f65fd38b4845db95f7684a
oJIeg <oleg151-13@mail.ru>
2022-10-11 18:56:36 +0300

    add copy Price for page

--- wwwroot/Copy.js
+++ wwwroot/Copy.js
@@ -1,3 +1,15 @@
-﻿function copyСlipboard() {
+﻿const tooltiptext = document.getElementById("tooltiptext");
+function copyСlipboard() {
 	navigator.clipboard.writeText('Стоимость изготовления на бумаге @Model.PaperResult.NamePaper - @Model.PaperResult.Price руб.')
+}
+
+function CopyPrice(price) {
+	navigator.clipboard.writeText(price)
+	tooltiptext.classList.remove("tooltiptext");
+	tooltiptext.classList.add("tooltiptext2");
+	setTimeout(editClass, 3000);
+}
+function editClass() {
+	tooltiptext.classList.remove("tooltiptext2");
+	tooltiptext.classList.add("tooltiptext");
 }
