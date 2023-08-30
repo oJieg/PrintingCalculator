5f330f52a5edbd013a02a3c21e8b0244a26b0ce5
oJieg <oleg151-13@mail.ru>
2023-08-30 21:21:04 +0300

    add migration

---
+++ Migrations/20230830181859_AddStapleBrochure.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace printing_calculator.Migrations
+{
+    public partial class AddStapleBrochure : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "StapleBrochure",
+                table: "InputsHistories",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StapleBrochurePrice",
+                table: "Histories",
+                type: "integer",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StapleBrochure",
+                table: "InputsHistories");
+
+            migrationBuilder.DropColumn(
+                name: "StapleBrochurePrice",
+                table: "Histories");
+        }
+    }
+}
