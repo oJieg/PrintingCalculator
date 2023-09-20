c0f2f8f30768a0a36d6ed3e0dc45530348281fa8
oJieg <oleg151-13@mail.ru>
2023-09-21 00:35:50 +0300

    add format Phone

--- wwwroot/DetalContact.js
+++ wwwroot/DetalContact.js
@@ -1,9 +1,12 @@
 async function detalContact(contactId) {
     $('#TableContactOrder > > .mainTableRow').empty();
     let contact = await getContact(contactId);
-    console.log(contact);
+    //console.log(contact);
     addInformatioContact(contact);
     await generatorTable(await listOrderForId(contact), 'TableContactOrder')
+
+    let event = new Event("GeneratorComplite", {bubbles: true}); // (2)
+    contactPhone.dispatchEvent(event);
 }
 
 async function editNemaContact(contactId){
@@ -37,7 +40,7 @@ function addInformatioContact(contact) {
     //contact.description);
     let allPhone = "";
     if (contact.phoneNmbers.$values.length > 0) {
-
+console.log(contact.phoneNmbers);
         for (let i = 0; i < contact.phoneNmbers.$values.length; i++) {
             allPhone += '<div class="horizontalFlex" id="opacitySize">' +
                 '<p class="contactText">' + contact.phoneNmbers.$values[i].number + '</p>' +
@@ -45,8 +48,8 @@ function addInformatioContact(contact) {
                 '</div>';
         }
     }
-    allPhone += '<p>добавить новый:</p>' +
-        '<input class="inputNumberContact" id="newPhoneContact' + contact.id + '" type="text" onchange="addPhoneContact(' + contact.id + ');">'
+    allPhone += '<p>добавить новый:</p> '  +
+        '<input class="tel" id="newPhoneContact' + contact.id + '" type="text" onchange="addPhoneContact(' + contact.id + ');">'
     $('#contactPhone').html(allPhone);
 
     let allMail = "";
@@ -69,7 +72,7 @@ async function listOrderForId(contact) {
     for (let i = 0; i < contact.orders.$values.length; i++) {
         listOrder[i] = await getOrderForId2(contact.orders.$values[i].id);
     }
-    console.log(listOrder)
+    //console.log(listOrder)
     return (listOrder);
 }
 
@@ -149,7 +152,7 @@ async function apiAddNewMailInContact(contactId, newMail){
 }
 
 async function apiDelMailContact(contactId, mail){
-    let respone1 = await fetch('https://localhost:7181/api/contact/del-mail'+contactId+'?eMail='+ mail, {
+    let respone1 = await fetch('/api/contact/del-mail'+contactId+'?eMail='+ mail, {
         method: "Get",
         headers: { "Accept": "application/json", "Content-Type": "application/json" },
     });
@@ -176,3 +179,43 @@ async function apiDelPhoneContact(contactId, phone){
 
     return responseOrder.result;
 }
+window.addEventListener("GeneratorComplite", function() {
+    [].forEach.call( document.querySelectorAll('.tel'), function(input) {
+      var keyCode;
+      function mask(event) {
+        event.keyCode && (keyCode = event.keyCode);
+        var pos = this.selectionStart;
+        if (pos < 3) event.preventDefault();
+        var matrix = "+7 (___) ___ ____",
+            i = 0,
+            def = matrix.replace(/\D/g, ""),
+            val = this.value.replace(/\D/g, ""),
+            new_value = matrix.replace(/[_\d]/g, function(a) {
+                return i < val.length ? val.charAt(i++) : a
+            });
+        i = new_value.indexOf("_");
+        if (i != -1) {
+            i < 5 && (i = 3);
+            new_value = new_value.slice(0, i)
+        }
+        var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+            function(a) {
+                return "\\d{1," + a.length + "}"
+            }).replace(/[+()]/g, "\\$&");
+        reg = new RegExp("^" + reg + "$");
+        if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) {
+          this.value = new_value;
+        }
+        if (event.type == "blur" && this.value.length < 5) {
+          this.value = "";
+        }
+      }
+  
+      input.addEventListener("input", mask, false);
+      input.addEventListener("focus", mask, false);
+      input.addEventListener("blur", mask, false);
+      input.addEventListener("keydown", mask, false);
+  
+    });
+  
+  });
